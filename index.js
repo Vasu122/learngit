7d528e4700c74ac7e99fde75c07c1f4b5f431ec1
Vasu122 <singhrabindra87@gmail.com>
2021-03-03 07:32:58 +0700

    Revert "i will be revert code"
    This reverts commit 508af96ee8909b7f56ecc5746d2185d2684c5d5e.

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ document.write("I am creating test page");
         <h1>Hello index</h1>
 
         i have checked revert command 111111111111111111111111111111
-
-
-        I am checking this file --222222222222222222222222222222222
